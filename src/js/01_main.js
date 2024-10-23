fc90ed8022295922abb562ae0c73dabb92e441c9
Maria Kovalenko <workmail358@gmail.com>
2024-10-23 21:26:58 +0300

    media queries

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     const button = document.querySelector('.burger-menu');
     const menu = document.querySelector('.header__nav');
-    const links = document.querySelectorAll('.header__nav .nav__link');
+    const links = document.querySelectorAll('.header__nav .nav__link'); 
+    const submitButton  = document.querySelector('.header__buttons'); 
   
     button.addEventListener('click', () => {
       button.classList.toggle('active');
       document.body.classList.toggle('open');
+      submitButton.classList.toggle('open');
   
       if (button.classList.contains('active')) {
         button.setAttribute('aria-expanded', 'true');
